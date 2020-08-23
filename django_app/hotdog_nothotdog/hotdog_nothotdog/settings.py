af334e12df1159bc59333e6fbd28966214bce70e
Ramanan <reachramzz25@gmail.com>
2020-08-23 18:23:44 +0530

    changed django app

--- django_app/hotdog_nothotdog/hotdog_nothotdog/settings.py
+++ django_app/hotdog_nothotdog/hotdog_nothotdog/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+dnm9$=r_tmg=+wo*3r6b7t#p763rffa^p@e_po&ura98y@+h8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
